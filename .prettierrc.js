425c90308c1ab6fff6663a4ee7f1da53ec4ab872
ConnectOn Test <connecton13@email.com>
2021-01-11 11:12:52 +0530

    enforcing coding style uisng eslint and prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,11 @@
+// .prettierrc.js
+
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
+  bracketSpacing: true,
   singleQuote: true,
   trailingComma: 'all',
 };
+
+// bracketSpacing - adding a space around brackets, e.g. import { useState } from 'react'; vs import {useState} from 'react';
+// singleQuote - using single quotes for strings, e.g. import React from 'react' vs import React from "react"
+// trailingComma - adding a trailing comma after array arguments (the default is not to have a trailing comma for the last element of the array)
